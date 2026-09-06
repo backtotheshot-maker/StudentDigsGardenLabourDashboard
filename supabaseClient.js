27145a972786177c00d69d8eaccd440db22a81e8
backtotheshot-maker <backtotheshot@gmail.com>
2026-09-06 14:39:19 +0100

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
-// supabase-js UMD build attaches `window.supabase` with a `.createClient` factory.
-export const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true },
-});
